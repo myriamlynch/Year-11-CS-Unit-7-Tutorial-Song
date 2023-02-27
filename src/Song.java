0bbbb268d783560737f05060f81456db57ff820b
myriamlynch <myriaml2023@student.cis.edu.hk>
2023-02-27 23:58:00 +0800

    completed

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,45 @@
+import java.util.ArrayList;
+
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+    private ArrayList<String> listeners;
+
+    public Song(String t, String a)
+    {
+        title = t;
+        artist = a;
+        listeners = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int howMany(ArrayList<String> arr)
+    {
+        int count = 0;
+        for(int i = 0; i < arr.size(); i++)
+        {
+            if(!listeners.contains(arr.get(i).toLowerCase())) {
+                listeners.add(arr.get(i).toLowerCase());
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
